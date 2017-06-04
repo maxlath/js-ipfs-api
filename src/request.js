2a12ccd9268cdd611925107a72b969aee7b93095
maxlath <k@maxlath.eu>
2017-06-04 21:45:19 +0200

    request: follow redirects

--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const httpRequest = require('http').request
-const httpsRequest = require('https').request
+const httpRequest = require('follow-redirects').http.request
+const httpsRequest = require('follow-redirects').https.request
 
 module.exports = (protocol) => {
   if (protocol.indexOf('https') === 0) {
